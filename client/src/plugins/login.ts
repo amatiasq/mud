c37c75919c015220f8b3cc45d62c9cbd03653ae6
A. Matías Quezada <amatiasq@gmail.com>
2020-11-07 03:02:25 +0100

    Fix login on reconnection

--- client/src/plugins/login.ts
+++ client/src/plugins/login.ts
@@ -9,14 +9,21 @@ export async function login(
   write(username);
   log('Username sent, waiting for password request');
 
+  // TODO: "Reconectando..." no triggerea esto nunca
+
   await when('Password:');
   write(password);
   log(`Logged in as ${username}`);
 
-  await when('Pulsa [ENTER]');
-  write('');
-  await when('Pulsa [ENTER]');
-  write('');
+  await Promise.any([
+    when('Reconectando.'),
+    (async () => {
+      await when('Pulsa [ENTER]');
+      write('');
+      await when('Pulsa [ENTER]');
+      write('');
+    })(),
+  ]);
 
   // Kids don't do this at home
   await when('Bienvenido a Balzhur...', initialize, { await: true }).once();
